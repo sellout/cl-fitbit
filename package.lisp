ad7e6f82e86f237d7872a307fccdd5cc4df355cb
Greg Pfeil <greg@technomadic.org>
2011-03-11 20:42:52 -0500

    Export everything.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,38 @@
 (defpackage fitbit
   (:use #:cl #:drakma #:oauth #:json #:split-sequence #:puri)
-  (:export #:activities-for)
-  (:shadow #:request))
+  (:shadow #:request)
+  (:export #:make-fitbit-consumer
+           #:get-authentication-url
+           #:get-authorized-user
+           
+           #:activities-for
+           #:recent-activities #:frequent-activities #:favorite-activities
+           #:profile
+           #:foods-for #:recent-foods #:frequent-foods #:favorite-foods
+           
+           #:calories-in
+
+           #:calories-out
+           #:steps
+           #:distance
+           #:time-sedentary
+           #:time-lightly-active
+           #:time-fairly-active
+           #:time-very-active
+           #:active-score
+
+           #:time-asleep
+           #:time-awake
+           #:awakenings
+           #:time-in-bed
+
+           #:weight
+           #:bmi
+           #:fat
+
+           #:activity
+           #:search-foods
+           #:food-units
+           
+           #:devices
+           #:device-attributes))
